8b08bfbf6339aa3c94782829fc5a6fea329f4db8
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-22 05:45:36 +0200

    Update run_deployment_script.sh

--- run_deployment_script.sh
+++ run_deployment_script.sh
@@ -1,1 +1,6 @@
-npx hardhat run scripts/deploy.js --network <fadaka>
+npx hardhat run scripts/deploy.js --network <fadaka-blockchain>
+
+cd Fadaka-blockchain/ai_tools
+python generate_readme.py       # Generates README.generated.md
+python docgen_py.py             # Generates .doc.md for Python files
+python docgen_go.py             # Generates .doc.md for Go files
