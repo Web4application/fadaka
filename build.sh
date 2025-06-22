a5d0e274837feed032c45042c0817772ac8389b4
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-22 04:12:03 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,36 @@
+$ ./btfs init
+Generating TRON key with BIP39 seed phrase...
+Master public key:  xpub661MyMwAqRbcGgHpeMqFkS5hnwoGeAcHG5KkDQwke7wFxtKqfsXTCTjWsoU2dYVXVGvV7EuGcviEzEJ143TezxxXvs2zZ9FYTtCei8iRQ66
+initializing BTFS node at /home/ubuntu/.btfs
+generating btfs node keypair with TRON key...done
+peer identity: 16Uiu2HAmKFQPM72SssFRrqcH1qwUsPwcp7vXSg3SEzfdYua1J5qc
+to get started, enter:
+
+	btfs cat /btfs/QmZjrLVdUpqVU6Pnc8pBnyQxVdpn9J8tfcsycP84W6N93C/readme
+
+$ ./btfs daemon
+Initializing daemon...
+go-btfs version: 2.0
+Repo version: 10
+System version: amd64/darwin
+Golang version: go1.16.5
+Repo location: /Users/btfs/.btfs
+Peer identity: 16Uiu2HAmKFQPM72SssFRrqcH1qwUsPwcp7vXSg3SEzfdYua1J5qc
+the address of Bttc format is:  0x7Cf4B71017F0312037D53fe966CE625BF98FFff6
+the address of Tron format is:  TMMuwwxsuQGrDrN3aanc5y5r4FbibgLYDa
+cannot continue until there is sufficient (100 Suggested) BTT (for Gas) available on 0x7cf4b71017f0312037d53fe966ce625bf98ffff6
+
+cannot continue until there is sufficient (100 Suggested) BTT (for Gas) available on 0x7cf4b71017f0312037d53fe966ce625bf98ffff6 
+self vault: 0x1f8b3e7e691d733f5eb17e5570c49de3e5aecef9 
+Swarm listening on /ip4/127.0.0.1/tcp/4001
+Swarm listening on /ip4/192.168.21.149/tcp/4001
+Swarm listening on /ip6/::1/tcp/4001
+Swarm listening on /p2p-circuit
+Swarm announcing /ip4/127.0.0.1/tcp/4001
+Swarm announcing /ip4/192.168.21.149/tcp/4001
+Swarm announcing /ip6/::1/tcp/4001
+API server listening on /ip4/127.0.0.1/tcp/5001
+Dashboard: http://127.0.0.1:5001/dashboard
+Gateway (readonly) server listening on /ip4/127.0.0.1/tcp/8080
+Remote API server listening on /ip4/127.0.0.1/tcp/5101
+Daemon is ready
