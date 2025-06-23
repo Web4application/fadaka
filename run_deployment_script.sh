c05f572004cf34fd75cdd23600bac18577151184
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-23 17:23:31 +0200

    Update run_deployment_script.sh

--- run_deployment_script.sh
+++ run_deployment_script.sh
@@ -4,3 +4,13 @@ cd Fadaka-blockchain/ai_tools
 python generate_readme.py       # Generates README.generated.md
 python docgen_py.py             # Generates .doc.md for Python files
 python docgen_go.py             # Generates .doc.md for Go files
+
+git clone https://github.com/Web4application/script_analyzer_bot.git
+cd script_analyzer_bot
+pip install -r requirements.txt
+
+
+## Running the API
+
+uvicorn app.main:app --reload
+http://127.0.0.1:8000/docs
