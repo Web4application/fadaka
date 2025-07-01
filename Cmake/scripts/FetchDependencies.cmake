2834cac66728073a65200320977864875b75c386
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-01 05:26:36 +0200

    Add files via upload

---
+++ Cmake/scripts/FetchDependencies.cmake
@@ -0,0 +1,8 @@
+include(FetchContent)
+# Example: fmtlib
+FetchContent_Declare(
+  fmt
+  GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+  GIT_TAG        10.2.1
+)
+FetchContent_MakeAvailable(fmt)
