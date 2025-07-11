f3a38c06277d162069eb016ea09697d982795828
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-12 01:04:23 +0200

    Update OpenERC1155Receiver.sol
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- contracts/OpenERC1155Receiver.sol
+++ contracts/OpenERC1155Receiver.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 
 contract OpenERC1155Receiver is IERC1155Receiver, ERC165 {
+    /// @notice Handles receipt of a single ERC1155 token type
     function onERC1155Received(
         address,
         address,
@@ -15,6 +16,7 @@ contract OpenERC1155Receiver is IERC1155Receiver, ERC165 {
         return this.onERC1155Received.selector;
     }
 
+    /// @notice Handles receipt of multiple ERC1155 token types (batch)
     function onERC1155BatchReceived(
         address,
         address,
@@ -22,14 +24,19 @@ contract OpenERC1155Receiver is IERC1155Receiver, ERC165 {
         uint256[] calldata,
         bytes calldata
     ) external pure override returns (bytes4) {
-        return this.onERC1155Received.selector;
+        return this.onERC1155BatchReceived.selector;
     }
 
+    /// @notice Supports IERC1155Receiver interface
     function supportsInterface(bytes4 interfaceId) public view override returns (bool) {
-        return interfaceId == type(IERC1155Receiver).interfaceId
-            || super.supportsInterface(interfaceId);
+        return
+            interfaceId == type(IERC1155Receiver).interfaceId ||
+            super.supportsInterface(interfaceId);
     }
 
+    /// @notice Accepts ETH fallback
     fallback() external payable {}
+
+    /// @notice Accepts ETH receive
     receive() external payable {}
 }
