53f12536b05708e3dc5692d7d1c15cceb977d01e
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-11 22:00:38 -0700

    Update .bat
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- SRBMiner-Multi-2-9-4/.bat
+++ SRBMiner-Multi-2-9-4/.bat
@@ -1,1 +1,3 @@
-SRBMiner-MULTI --algorithm ethash --pool us-south01.miningrigrentals.com3344 --wallet web4.331855 --password x
+SRBMiner-MULTI.exe --algorithm ethash --pool us-south01.miningrigrentals.com:3344 --wallet web4.331855 --password x --gpu-boost 5
+pause
+
