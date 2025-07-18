7e6da216f02a81712f5bd068e2b417fd510da269
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-18 21:38:52 +0200

    Create .bash
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

---
+++ .github/internal/immutable-actions/actions/deploy-pages/download/.bash
@@ -0,0 +1,2 @@
+$ sha256:d8bbdde09dc69a9a8e1a777947476a005b763afc47f2167b683872bd589b3851?x-ghcr-actor-id=1005488806&x-ghcr-actor-type=repository&x-ghcr-alg=hmac-sha256&x-ghcr-date=2025-07-08T02:03:06Z&x-ghcr-expires=10m0s&x-ghcr-host=ghcr.io
+
