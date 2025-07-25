b39bb7ea0daf7d034e3e0ed6bb4465f29134ee8a
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-25 18:03:13 +0200

    Create fvm.go

---
+++ vm/fvm.go
@@ -0,0 +1,37 @@
+package vm
+
+import (
+    "fmt"
+)
+
+type VM struct {
+    PC     int
+    Stack  []uint64
+    Code   []byte
+    Gas    uint64
+    Halted bool
+}
+
+func NewFVM(code []byte, gas uint64) *VM {
+    return &VM{
+        PC:     0,
+        Stack:  make([]uint64, 0),
+        Code:   code,
+        Gas:    gas,
+        Halted: false,
+    }
+}
+
+func (vm *VM) Run() {
+    for !vm.Halted && vm.PC < len(vm.Code) {
+        op := vm.Code[vm.PC]
+        vm.PC++
+
+        if handler, ok := opcodeTable[op]; ok {
+            handler(vm)
+        } else {
+            fmt.Printf("🚫 Unknown opcode: 0x%x\n", op)
+            vm.Halted = true
+        }
+    }
+}
