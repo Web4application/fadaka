efb1fe26402819d4edafe1d86cb4577d5a80fd5f
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-25 18:29:58 +0200

    Update fadaka_vm.go

--- fvm/fadaka_vm.go
+++ fvm/fadaka_vm.go
@@ -1,35 +1,39 @@
-package fvm
-
-import (
-	"fmt"
-	"github.com/wasmerio/wasmer-go/wasmer"
-)
-
-type FVM struct {
-	engine *wasmer.Engine
-	store  *wasmer.Store
-}
-
-func NewFVM() *FVM {
-	engine := wasmer.NewEngine()
-	store := wasmer.NewStore(engine)
-	return &FVM{engine, store}
-}
-
-func (fvm *FVM) ExecuteWASM(wasmBytes []byte, entry string, args ...int32) error {
-	module, err := wasmer.NewModule(fvm.store, wasmBytes)
+func (fvm *FVM) ExecuteContract(wasm []byte, entry string, args ...int32) error {
+	module, err := wasmer.NewModule(fvm.store, wasm)
 	if err != nil {
-		return err
+		return fmt.Errorf("WASM compile error: %w", err)
 	}
-	importObject := wasmer.NewImportObject()
-	instance, err := wasmer.NewInstance(module, importObject)
+
+	// Create WASM import for `fadaka_log`
+	fadakaLog := wasmer.NewFunction(
+		fvm.store,
+		wasmer.NewFunctionType(
+			wasmer.NewValueTypes(wasmer.I32), []wasmer.ValueType{}),
+		func(args []wasmer.Value) ([]wasmer.Value, error) {
+			logVal := args[0].I32()
+			fmt.Printf("[FVM Log] Contract says: %d\n", logVal)
+			return nil, nil
+		},
+	)
+
+	imports := wasmer.NewImportObject()
+	imports.Register(
+		"env",
+		map[string]wasmer.IntoExtern{
+			"fadaka_log": fadakaLog,
+		},
+	)
+
+	instance, err := wasmer.NewInstance(module, imports)
 	if err != nil {
-		return err
+		return fmt.Errorf("WASM instantiation error: %w", err)
 	}
+
 	function, err := instance.Exports.GetFunction(entry)
 	if err != nil {
-		return err
+		return fmt.Errorf("entry '%s' not found: %w", entry, err)
 	}
+
 	_, err = function(args...)
 	return err
 }
