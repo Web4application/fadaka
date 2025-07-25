2e9d6791671e8281c3c9dc2d940dcc14f4010c32
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-25 18:05:41 +0200

    Update opcodes.go

--- vm/opcodes.go
+++ vm/opcodes.go
@@ -37,3 +37,26 @@ func (vm *VM) pop() uint64 {
     vm.Stack = vm.Stack[:len(vm.Stack)-1]
     return val
 }
+
+var opcodeTable = map[byte]func(*VM){
+    0x00: func(vm *VM) { vm.Halted = true }, // STOP
+    0x01: func(vm *VM) { vm.push(vm.pop() + vm.pop()) }, // ADD
+    0x02: func(vm *VM) { vm.push(vm.pop() * vm.pop()) }, // MUL
+    0x03: func(vm *VM) { b := vm.pop(); a := vm.pop(); vm.push(a - b) }, // SUB
+    0x04: func(vm *VM) { b := vm.pop(); a := vm.pop(); if b != 0 { vm.push(a / b) } else { vm.Halted = true } }, // DIV
+    0x05: func(vm *VM) { b := vm.pop(); a := vm.pop(); if b != 0 { vm.push(a % b) } else { vm.Halted = true } }, // MOD
+    0x10: func(vm *VM) { b := vm.pop(); a := vm.pop(); if a < b { vm.push(1) } else { vm.push(0) } }, // LT
+    0x11: func(vm *VM) { b := vm.pop(); a := vm.pop(); if a > b { vm.push(1) } else { vm.push(0) } }, // GT
+    0x14: func(vm *VM) { b := vm.pop(); a := vm.pop(); if a == b { vm.push(1) } else { vm.push(0) } }, // EQ
+    0x50: func(vm *VM) { _ = vm.pop() }, // POP
+    0x56: func(vm *VM) { vm.PC = int(vm.pop()) }, // JUMP
+    0x57: func(vm *VM) {
+        dest := vm.pop()
+        cond := vm.pop()
+        if cond != 0 {
+            vm.PC = int(dest)
+        }
+    }, // JUMPI
+    0x60: func(vm *VM) { vm.push(uint64(vm.Code[vm.PC])); vm.PC++ }, // PUSH1
+    0xfe: func(vm *VM) { fmt.Println("🚨 INVALID OPCODE"); vm.Halted = true },
+}
