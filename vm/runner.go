e7e1c9d5654061b0fa23038ab53f41b365bdd5e2
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-25 18:44:54 +0200

    Update runner.go

--- vm/runner.go
+++ vm/runner.go
@@ -34,3 +34,34 @@ func main() {
 	result, _ := helloFunc()
 	fmt.Println("[FADAKA RESULT]", result)
 }
+
+var storage = map[int32]int32{}
+
+fadakaStorageWrite := wasmer.NewFunction(
+	store,
+	wasmer.NewFunctionType([]*wasmer.ValueType{wasmer.I32, wasmer.I32}, []*wasmer.ValueType{}),
+	func(args []wasmer.Value) ([]wasmer.Value, error) {
+		key := args[0].I32()
+		val := args[1].I32()
+		storage[key] = val
+		fmt.Printf("[STORAGE WRITE] key=%d val=%d\n", key, val)
+		return nil, nil
+	},
+)
+
+fadakaStorageRead := wasmer.NewFunction(
+	store,
+	wasmer.NewFunctionType([]*wasmer.ValueType{wasmer.I32}, []*wasmer.ValueType{wasmer.I32}),
+	func(args []wasmer.Value) ([]wasmer.Value, error) {
+		key := args[0].I32()
+		val := storage[key]
+		fmt.Printf("[STORAGE READ] key=%d -> val=%d\n", key, val)
+		return []wasmer.Value{wasmer.NewI32(val)}, nil
+	},
+)
+
+imports.Register("env", map[string]wasmer.IntoExtern{
+	"fadaka_log": fadakaLog,
+	"fadaka_storage_write": fadakaStorageWrite,
+	"fadaka_storage_read": fadakaStorageRead,
+})
