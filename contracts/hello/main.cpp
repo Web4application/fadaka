765d053310129aead6629448d34310f55432b69e
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-25 18:43:12 +0200

    Update main.cpp

--- contracts/hello/main.cpp
+++ contracts/hello/main.cpp
@@ -7,3 +7,17 @@ extern "C" int hello() {
     fadaka_log(2025);
     return 2025;
 }
+
+extern "C" {
+    void fadaka_log(int val);
+    void fadaka_storage_write(int key, int value);
+    int fadaka_storage_read(int key);
+}
+
+extern "C" int hello() {
+    int key = 42;
+    fadaka_storage_write(key, 2025);
+    int stored = fadaka_storage_read(key);
+    fadaka_log(stored);
+    return stored;
+}
