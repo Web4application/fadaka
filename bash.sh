1cbe4602916b8f80e4c2946c389663361cdf4224
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-25 17:58:38 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -167,3 +167,5 @@ pnpm install
 git apply fix-typechain-version.patch
 
 go install github.com/ethereum/go-ethereum/cmd/abigen@latest
+
+go run assembly/cli.go decode 0x6080604052348015600f57600080fd5b
