02361d22e73be186f92c408d32f1270995ea5dc8
Web4 <167559384+Web4application@users.noreply.github.com>
2025-08-17 06:31:23 +0100

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Home from "./Home.vue";
+import { Web3AuthProvider } from "@web3auth/modal/vue";
+import { WagmiProvider } from "@web3auth/modal/vue/wagmi";
+import web3AuthContextConfig from "./web3authContext";
+
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col">
+    <Web3AuthProvider :config="web3AuthContextConfig">
+		<WagmiProvider>
+			<Home />
+		</WagmiProvider>
+    </Web3AuthProvider>
+  </div>
+</template>
