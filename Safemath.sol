dd8d82667a75e7929855202f7e74c91b446bae59
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-02 23:12:58 +0100

    Create Safemath.sol

---
+++ Safemath.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MirrorToken is ERC20, Ownable {
+    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
+}
